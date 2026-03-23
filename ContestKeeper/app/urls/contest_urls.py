7bdcf17600a2e3c7c16047aa6a687a4064401105
yourbrolol <andriy.bondar.2203@gmail.com>
2026-03-23 21:49:27 +0200

    check the previous commit, done the same for jurys view.

--- ContestKeeper/app/urls/contest_urls.py
+++ ContestKeeper/app/urls/contest_urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("new/",                                views.ContestFormView.as_view(),   name="contest_create"),
     path("<int:pk>/",                           views.ContestDetailView.as_view(), name="contest_detail"),
     path("<int:pk>/teams/",                     views.ViewTeamsView.as_view(),     name="contest_teams"),
+    path("<int:pk>/jurys/",                     views.ViewJurysView.as_view(),     name="contest_jurys"),
     path("<int:pk>/teams/<int:ck>/",            views.TeamDetailView.as_view(),    name="team_detail"),
     path("<int:pk>/edit/",                      views.ContestFormView.as_view(),   name="contest_edit"),
     path("<int:pk>/delete/",                    views.ContestDeleteView.as_view(), name="contest_delete"),
