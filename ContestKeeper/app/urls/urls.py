f533e01fe0ad2a50a13797945059dd26a97f8925
yourbrolol <andriy.bondar.2203@gmail.com>
2026-03-23 19:43:57 +0200

    changes in views.py and url routing; fixed the font in certain html files.

--- ContestKeeper/app/urls/urls.py
+++ ContestKeeper/app/urls/urls.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from .. import views
 from django.urls import path, include
 
@@ -10,4 +11,17 @@ urlpatterns = [
     path("contests/", include("app.urls.contest_urls")),
     path("applications/", include("app.urls.application_urls")),
     path('contests/<int:pk>/teams/', views.ViewTeamsView.as_view(), name='teams'),
-]
+]
+=======
+from .. import views
+from django.urls import path, include
+
+urlpatterns = [
+    path("", views.HomeView.as_view(), name="home"),
+    path("dashboard/", views.DashboardView.as_view(), name="dashboard"),
+    path("register/", views.RegisterView.as_view(), name="register"),
+    path("profile/", views.ProfileView.as_view(), name="profile"),
+    path("contests/", include("app.urls.contest_urls")),
+    path("applications/", include("app.urls.application_urls")),
+]
+>>>>>>> 2dcf23c (changes in views.py and url routing; fixed the font in certain html files.)
