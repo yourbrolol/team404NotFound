d94da0a21ebb6880d231162bc4fd262a043def21
yourbrolol <andriy.bondar.2203@gmail.com>
2026-03-23 21:53:44 +0200

    small changes.

--- ContestKeeper/app/urls/application_urls.py
+++ ContestKeeper/app/urls/application_urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path("<int:pk>/approve/", views.ApplicationActionView.as_view(), {"action": "approve"}, name="approve_application"),
-    path("<int:pk>/reject/",  views.ApplicationActionView.as_view(), {"action": "reject"},  name="reject_application"),
+    path("<int:pk>/reject/", views.ApplicationActionView.as_view(), {"action": "reject"},  name="reject_application"),
 ]
