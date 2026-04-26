496ebab97da330f77ff2781371e60cab9524557d
yourbrolol <andriy.bondar.2203@gmail.com>
2026-04-26 18:02:01 +0300

    update requirements.txt

---
+++ launch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Script to launch the ContestKeeper project locally
+
+# Get the directory where the script is located
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$DIR"
+
+# Change to the application directory
+cd ContestKeeper || exit 1
+
+# Start the Django development server
+echo "Starting server on http://127.0.0.1:8000/..."
+python manage.py runserver
