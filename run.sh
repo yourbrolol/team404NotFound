ed486137ed50f022365e2cccb9c3498420129fdb
yourbrolol <andriy.bondar.2203@gmail.com>
2026-05-01 20:42:19 +0300

    add shortcut: run.sh script

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [ -d "$DIR/.lvenv" ]; then
+    . "$DIR/.lvenv/bin/activate"
+    bash "$DIR/launch.sh"
+elif [ -d "$DIR/.venv" ]; then
+    . "$DIR/.venv/bin/activate"
+    bash "$DIR/launch.sh"
+else
+    echo "The venv names don't match; check if you have a .venv / .lvenv folder or edit the script."
+fi
